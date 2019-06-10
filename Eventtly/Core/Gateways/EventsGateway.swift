514d31f107834f6ce2c079d7506ecdeadc46a2c9
KarimEbrahemAbdelaziz <karimabdelazizmansour@gmail.com>
2019-06-10 14:46:27 +0200

    Add events gateway

---
+++ Eventtly/Core/Gateways/EventsGateway.swift
@@ -0,0 +1,17 @@
+//
+//  EventsGateway.swift
+//  Eventtly
+//
+//  Created by Karim Ebrahem on 6/10/19.
+//  Copyright © 2019 Karim Ebrahem. All rights reserved.
+//
+
+import Foundation
+
+typealias FetchEventsCategoriesEntityGatewayCompletionHandler = (_ categories: Result<[EventCategory], Error>) -> Void
+typealias FetchEventDetailsEntityGatewayCompletionHandler = (_ categories: Result<Event, Error>) -> Void
+
+protocol EventsGateway {
+    func fetchCategories(completionHandler: @escaping FetchEventsCategoriesEntityGatewayCompletionHandler)
+    func fetchEventDetails(completionHandler: @escaping FetchEventDetailsEntityGatewayCompletionHandler)
+}
